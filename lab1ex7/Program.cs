4d463924776cdbef0071e136a78c207fdf744dc1
Borze Andrei <borzeandrew@gmail.com>
2022-11-26 22:04:07 +0200

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab1ex7
+{
+    /*Scrieti un program care va verifica daca un numar citit de la tastatura este par sau impar*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acest program verifica daca numarul citit este par sau impar. \n");
+
+            Console.WriteLine("Introduceti un numar:");
+            var nr = int.Parse(Console.ReadLine());
+
+            if (nr % 2 == 0)
+                Console.WriteLine("Par");
+            else
+                Console.WriteLine("Impar");
+
+        }
+    }
+}
